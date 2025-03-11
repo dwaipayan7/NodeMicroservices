37b6f3ee971b548b6542c906609e1a8b764eac3d
Dwaipayan Biswas <biswastatay73@gmail.com>
2025-03-11 21:58:41 +0530

    Add login functionality with JWT authentication

--- auth_micro/controller/auth.controller.js
+++ auth_micro/controller/auth.controller.js
@@ -1,5 +1,8 @@
 import bcrypt from 'bcrypt';
 import prisma from '../config/db.config.js';
+import jwt from 'jsonwebtoken';
+import { config } from 'dotenv';
+config();
 
 class AuthController {
     static async register(req, res) {
@@ -33,6 +36,51 @@ class AuthController {
             return res.status(500).json({ error: "Internal server error", details: error.message });
         }
     }
+
+
+    static async login(req, res) {
+        try {
+
+            const {email, password} = req.body;
+
+            if (!email || !password) {
+                return res.status(400).json({ error: "All fields (email, password) are required" });
+            }
+
+            const user = await prisma.user.findUnique({
+                where: { email }
+            });
+
+            if (!user) {
+                return res.status(404).json({ error: "User not found" });
+            }
+
+            if(!bcrypt.compareSync(password, user.password)) {
+                return res.status(400).json({ error: "Invalid credentials" });
+            }
+
+            const payload = {
+                id: user.id,
+                name:user.name,
+                email:user.email
+            }
+
+            const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '7d' });
+
+            return res.status(200).json({
+                message: "Login successful",
+                token
+            });
+
+            
+        } catch (error) {
+            console.log(error);
+            return res.status(500).json({ error: "Internal server error", details: error.message });
+            
+        }
+    }
+
+
 }
 
 export default AuthController;
--- auth_micro/routes/auth.routes.js
+++ auth_micro/routes/auth.routes.js
@@ -5,6 +5,7 @@ const router = Router();
 
 
 router.post("/auth/register",AuthController.register);
+router.post("/auth/login",AuthController.login);
 
 
 export default router;
