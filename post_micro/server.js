0866f3ee6c885cb750c878c3c0f8dfc27575734b
Dwaipayan Biswas <biswastatay73@gmail.com>
2025-03-14 18:50:43 +0530

    Initialize post_micro project with Express, Prisma, and JWT authentication

--- post_micro/server.js
+++ post_micro/server.js
@@ -2,7 +2,7 @@ import { config } from 'dotenv';
 import express from 'express';
 config();
 import cors from 'cors';
-import Routes from './routes/index.js';
+// import Routes from './routes/index.js';
 
 const app = express();
 
@@ -11,15 +11,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //router
-app.use(Routes);
+// app.use(Routes);
 
 app.get("/", (req, res) =>{
     res.send("Dwaipayan");
 });
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 4002;
 
 app.listen(PORT, () =>{
-    console.log("Server is running on port 3000");
+    console.log(`Server is running on port ${PORT}`);
     
 });
